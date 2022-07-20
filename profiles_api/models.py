4da9d71d9b979d62f06dfd745ceb9584e43eb11c
roshcg <rgnaik96@gmail.com>
2022-07-21 00:49:43 +0530

    Added custom user profile model, manager and mgrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,69 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """
+    Manager class for user profile
+    """
+    
+    def create_user(self, email, name, password=None):
+        """
+        create a new user profiles_api
+        """
+        if not email:
+            raise ValueError('User must have an email')
+
+        email = self.normailze_email(email)
+        user = self.model(name=name, email=email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """
+        creates super user and save with given details
+        """
+        user = create_user
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Database model for users in system.
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
+
+    def get_full_name(self):
+        """
+        Retreive full name of the user.
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        Retrieve short name of the user.
+        """
+        return self.name
+
+    def __str__(self):
+        """
+        Return the string username of user
+        """
+        return self.email
